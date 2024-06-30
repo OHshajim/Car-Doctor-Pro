ee7624ea2e66731a10385055e6fcb84a4b2ad021
OHshajim <ajshajimmax@gmail.com>
2024-06-30 14:44:31 +0600

    file setup

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+
+      <h2 className={`mb-3 text-2xl font-semibold`}>
+        Car doctor pro
+      </h2>
+      <button className="btn btn-outline">Click</button>
+    </main>
+  );
+}
