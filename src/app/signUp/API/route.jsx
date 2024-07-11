1614eec8e4f2091cb83b059ebaf1fdc7e97c49d7
OHshajim <ajshajimmax@gmail.com>
2024-07-11 23:16:40 +0600

    user condition applied

--- src/app/signUp/API/route.jsx
+++ src/app/signUp/API/route.jsx
@@ -5,9 +5,18 @@ export const POST = async (request) => {
   try {
     const db = await ConnectDB();
     const userCollection = db.collection("Users");
+    const check = await userCollection.find({ email: newUser.email });
+    if(check){
+      return Response.json({ message: "User Already exist" }, { status: 304 });
+    }
     const res = await userCollection.insertOne(newUser);
     return Response.json({ message: "User Created" }, { status: 200 });
-  } catch (error) {
-    console.log(error);
+
+  }
+   catch (error) {
+    // return Response.json(
+    //   { message: "Something going wrong , try again !!!" }
+    //   // { status: 500 }
+    // );
   }
 };
