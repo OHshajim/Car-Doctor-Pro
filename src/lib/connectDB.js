543f2a93ad0be0b1999c81ad5b7bdb6a40221708
OHshajim <ajshajimmax@gmail.com>
2024-07-11 22:33:47 +0600

    fixed the mongoDB connect issue

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -5,14 +5,14 @@ export const ConnectDB = () => {
        if (db) return db ;
        try {
         const url = process.env.MONGODB_URI
-        const client = new MongoClient(uri, {
+        const client = new MongoClient(url, {
             serverApi: {
               version: ServerApiVersion.v1,
               strict: true,
               deprecationErrors: true,
             }
           });
-          db = client.db('car-doctor')
+          db = client.db('CarDoctor')
           return db;
        } catch (error) {
         console.log(error);
