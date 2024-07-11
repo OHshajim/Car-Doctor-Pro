bec6f0d445a6981652a5b88f4becac637d88867a
OHshajim <ajshajimmax@gmail.com>
2024-07-12 02:37:06 +0600

    user condition fixed

--- src/app/signUp/API/route.jsx
+++ src/app/signUp/API/route.jsx
@@ -5,18 +5,17 @@ export const POST = async (request) => {
   try {
     const db = await ConnectDB();
     const userCollection = db.collection("Users");
-    const check = await userCollection.find({ email: newUser.email });
-    if(check){
-      return Response.json({ message: "User Already exist" }, { status: 304 });
+    const check = await userCollection.findOne({ email: newUser.email });
+    if (check) {
+      console.log("to", check, "from");
+      return Response.json({ message: "User Already exist" }, { status: 200 });
     }
     const res = await userCollection.insertOne(newUser);
     return Response.json({ message: "User Created" }, { status: 200 });
-
-  }
-   catch (error) {
-    // return Response.json(
-    //   { message: "Something going wrong , try again !!!" }
-    //   // { status: 500 }
-    // );
+  } catch (error) {
+    return Response.json(
+      { message: "Something going wrong , try again !!!", error },
+      { status: 500 }
+    );
   }
 };
